38cebe107cfd37750c7bd39a9c6c21152c0088d7
Vhico Putra Pratama <vhicoputra.vp@gmail.com>
2020-05-28 21:21:35 +0700

    menambahkan efek parallax di js(not working)

--- js/script.js
+++ js/script.js
@@ -14,3 +14,21 @@ $('.page-scroll').on('click', function(e){
 
     e.preventDefault();
 });
+
+
+// menyimpan parallax
+$(window).scroll(function() {
+     var wScroll = $(this).scrollTop();
+
+     $('.jumbotron img').css({
+         'transfrom' : 'translate(0px, '+ wScroll/4 +'%)'
+     });
+
+     $('.jumbotron h1').css({
+         'transfrom' : 'translate(0px, '+ wScroll/2 +'%)'
+     });
+
+     $('.jumbotron p').css({
+         'transfrom' : 'translate(0px, '+ wScroll/1.2 +'%)'
+     });
+});
